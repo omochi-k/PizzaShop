728fd0db37d9d34a16c40f6a1615089048dc92f7
omochi-k <g10925ky+git@gm.tsuda.ac.jp>
2014-05-28 18:48:45 +0900

    注文の編集と削除を追加しました。 （データがとりだせない症状はそのままです。）

--- PizzaShop/src/s/pizza/PizzaShopServlet.java
+++ PizzaShop/src/s/pizza/PizzaShopServlet.java
@@ -48,9 +48,9 @@ public class PizzaShopServlet extends HttpServlet {
         String res = "[";
         if (list != null){
             for(OrderData data:list){
-                res += "{id:" + data.getId() + ",url:'" + data.getUrl() + "',title:'" +
-                    data.getTitle() + "',date:'" + data.getDatetime() +
-                    "',comment:'" + data.getComment() + "'},";
+                res += "{id:" + data.getId() + ",url:'" + data.getUrl() + "',order:'" +
+                        data.getOrder() + "',date:'" + data.getDatetime() +
+                        "',comment:'" + data.getComment() + "'},";
             }
         }
         res += "]";
